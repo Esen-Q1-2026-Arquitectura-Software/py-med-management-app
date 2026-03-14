a7d1b73bbeacdb6a78c93db2fa179e038784cb60
Balbino Aylagas <38702996+baylagas@users.noreply.github.com>
2026-03-14 11:35:49 -0600

    comment port on fe, be

--- be/app.py
+++ be/app.py
@@ -10,3 +10,8 @@ app.include_router(usuario_router.router)
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+    """ _port issues_
+        Development (auto-reload)
+        python -m uvicorn main:app --reload --host 127.0.0.1 --port 8000
+    """
--- fe/main.py
+++ fe/main.py
@@ -151,3 +151,4 @@ def registro():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # app.run(debug=True, port=5000)
